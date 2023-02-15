896d1de72f444458af38d8a0bb764708fb2a2778
Yrmee <y.eggermond@gmail.com>
2023-02-15 16:33:12 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import datetime
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html", utc_dt=datetime.datetime.now().strftime("%d %B %Y %H:%M"))
+
+@app.route("/<page_name>")
+def other_page(page_name): 
+    response = make_response(render_template("404.html", page_name=page_name), 404)
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test_app.py
@@ -0,0 +1,12 @@
+#import pytest
+from app import app
+
+def test_home_route():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    # assert response.data.decode('utf-8') == "Welcome to my Flask App."
+
+def test_other_page_route():
+    response = app.test_client().get('/<page_name>')
+    assert response.status_code == 404
+    # assert response.data.decode('utf-8') == "This page named <page_name> does not exist."
